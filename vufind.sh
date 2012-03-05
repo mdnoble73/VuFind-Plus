33766983b5d04f76e9181828a4214c14b3fd71c0
Mark Noble <mnoble@turningleaftech.com>
2012-03-05 08:47:38 -0700

    Update to allow JAVA OPTIONS to be passed to vufind.sh if not provided

--- vufind.sh
+++ vufind.sh
@@ -107,7 +107,10 @@ findDirectory()
 # Set Performance options for JETTY
 ##################################################
 #JAVA_OPTIONS="-server -Xms1048576k -Xmx1048576k -XX:+UseParallelGC -XX:NewRatio=5"
-JAVA_OPTIONS="-server -Xms1024m -Xmx1024m -XX:+UseParallelGC -XX:NewRatio=5"
+if [ -z "$VUFIND_HOME" ]
+then
+  JAVA_OPTIONS="-server -Xms1024m -Xmx1024m -XX:+UseParallelGC -XX:NewRatio=5"
+fi
 
 ##################################################
 # Set VUFIND_HOME
