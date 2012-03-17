2fd10af94c99c54fd7140dd4fb7c73ff48ef810a
Mark Noble <mnoble@turningleaftech.com>
2012-03-17 13:53:18 -0600

    Additional default files to make setup easier.

--- sites/default/default.sh
+++ sites/default/default.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 # set local configuration for starting Solr and then start solr
-export VUFIND_HOME=/usr/local/VuFind-Plus/vufind
-export JETTY_HOME=/usr/local/VuFind-Plus/jetty
-export SOLR_HOME=/usr/local/VuFind-Plus/solr-data/{libraryname}     
-export JETTY_PORT={libraryport}
+#Replace {servername} with your server name and save in sites/{servername} as {servername.sh} 
+export VUFIND_HOME=/usr/local/VuFind-Plus/sites/{sitename}
+export JETTY_HOME=/usr/local/VuFind-Plus/sites/{sitename}/solr/jetty
+export SOLR_HOME=/usr/local/VuFind-Plus/sites/{sitename}/solr     
+export JETTY_PORT=8080
+#Max memory should be at least he size of all solr indexes combined. 
 export JAVA_OPTIONS="-server -Xms1024m -Xmx6144m -XX:+UseParallelGC -XX:NewRatio=5"
-export JETTY_LOG=/usr/local/VuFind-Plus/logs/{libraryname}l
+export JETTY_LOG=/usr/local/VuFind-Plus/sites/{sitename}/logs/jetty
 
-exec /usr/local/VuFind-Plus/vufind.sh $1 $2
+exec /usr/local/VuFind-Plus/sites/default/vufind.sh $1 $2
