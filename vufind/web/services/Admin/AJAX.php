b28e2d3e05a8e1af3cf1ac76197feb04a6822fec
Mark Noble <mnoble@turningleaftech.com>
2012-04-19 17:21:39 -0600

    Fix loading list widgets that use search

---
+++ vufind/web/services/Admin/AJAX.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ *
+ * Copyright (C) Villanova University 2007.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ */
+
+require_once 'Action.php';
+
+class AJAX extends Action {
+
+	function launch() {
+		global $timer;
+		$method = $_GET['method'];
+		$timer->logTime("Starting method $method");
+		if (in_array($method, array())){
+			header('Content-type: text/plain');
+			header('Cache-Control: no-cache, must-revalidate'); // HTTP/1.1
+			header('Expires: Mon, 26 Jul 1997 05:00:00 GMT'); // Date in the past
+			echo $this->$method();
+		}else if (in_array($method, array('GetReindexProcessDetails'))){
+			header('Content-type: text/html');
+			header('Cache-Control: no-cache, must-revalidate'); // HTTP/1.1
+			header('Expires: Mon, 26 Jul 1997 05:00:00 GMT'); // Date in the past
+			echo $this->$method();
+
+		}else{
+			header ('Content-type: text/xml');
+			header('Cache-Control: no-cache, must-revalidate'); // HTTP/1.1
+			header('Expires: Mon, 26 Jul 1997 05:00:00 GMT'); // Date in the past
+
+			$xmlResponse = '<?xml version="1.0" encoding="UTF-8"?' . ">\n";
+			$xmlResponse .= "<AJAXResponse>\n";
+			if (is_callable(array($this, $_GET['method']))) {
+				$xmlResponse .= $this->$_GET['method']();
+			} else {
+				$xmlResponse .= '<Error>Invalid Method</Error>';
+			}
+			$xmlResponse .= '</AJAXResponse>';
+
+			echo $xmlResponse;
+		}
+	}
+
+	
+}
+?>
