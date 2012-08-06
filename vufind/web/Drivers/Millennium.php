790269e3fd3fd3e62615b949cd3896de41cbb0b1
Mark Noble <mnoble@turningleaftech.com>
2012-08-06 12:05:51 -0600

    Trim courier codes from on order records

--- vufind/web/Drivers/Millennium.php
+++ vufind/web/Drivers/Millennium.php
@@ -569,7 +569,7 @@ class MillenniumDriver implements DriverInterface
 		if (preg_match_all('/<tr\\s+class="bibOrderEntry">.*?<td\\s*>(.*?)<\/td>/s', $millenniumInfo->framesetInfo, $orderMatches)){
 			for ($i = 0; $i < count($orderMatches[1]); $i++) {
 				$location = trim($orderMatches[1][$i]);
-				$location = preg_replace('/\\sC\\d{3}\\s/', '', $location);
+				$location = preg_replace('/\\sC\\d{3}[\\s\\.]/', '', $location);
 				//Remove courier code if any
 				$sorted_array['7' . $location . $i] = array(
                     'location' => $location,
