decd3343ab05ce05a34e27f25609e02b374f7c17
Mark Noble <mnoble@turningleaftech.com>
2012-09-17 13:21:16 -0600

    Fix student login for students with 6 digit barcodes

--- vufind/web/Drivers/Millennium.php
+++ vufind/web/Drivers/Millennium.php
@@ -2919,6 +2919,8 @@ class MillenniumDriver implements DriverInterface
 		global $user;
 		if (strlen($user->cat_password) == 5){
 			$user->cat_password = '41000000' . $user->cat_password;
+		}elseif (strlen($user->cat_password) == 6){
+			$user->cat_password = '4100000' . $user->cat_password;
 		}
 		return $user->cat_password;
 	}
