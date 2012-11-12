04b7e678dcd64c6774bbcd5abaaaceeca0b1d0aa
Mark Noble <mnoble@turningleaftech.com>
2012-11-12 13:20:01 -0700

    Updates to database optimization

--- vufind/web/optimizeDatabase.php
+++ vufind/web/optimizeDatabase.php
@@ -7,6 +7,7 @@ require_once 'sys/Utils/SwitchDatabase.php';
 require_once 'sys/Timer.php';
 global $timer;
 $timer = new Timer();
+global $logger;
 $logger = new Logger();
 
 if ($configArray['System']['debug']) {
@@ -23,7 +24,7 @@ $library = new Library();
 $library->find();
 
 ob_start();
-echo("<br>Starting to optimize tables\r\n");
+echo("<br>Starting to optimize tables<br/>\r\n");
 $logger->log('Starting to optimize tables', PEAR_LOG_INFO);
 ob_flush();
 
@@ -35,37 +36,40 @@ foreach ($configArray['Database'] as $key => $value){
 			SwitchDatabase::switchToEcontent();
 		}
 		$tableName = $matches[1];
-		set_time_limit(300);
-		mysql_query("OPTIMIZE TABLE $tableName;");
-		$logger->log('Optimized table: ' . $tableName, PEAR_LOG_INFO);
+
+		optimizeTable($tableName);
+
 	}
 }
 
 //Optimize tables that are not part of the browse definition
 SwitchDatabase::switchToVuFind();
 set_time_limit(300);
-mysql_query('OPTIMIZE TABLE title_browse;');
-mysql_query('OPTIMIZE TABLE title_browse_metadata;');
-mysql_query('OPTIMIZE TABLE title_browse_scoped_results_global;');
-set_time_limit(300);
-mysql_query('OPTIMIZE TABLE author_browse;');
-mysql_query('OPTIMIZE TABLE author_browse_metadata;');
-mysql_query('OPTIMIZE TABLE author_browse_scoped_results_global;');
-set_time_limit(300);
-mysql_query('OPTIMIZE TABLE subject_browse;');
-mysql_query('OPTIMIZE TABLE subject_browse_metadata;');
-mysql_query('OPTIMIZE TABLE subject_browse_scoped_results_global;');
-set_time_limit(300);
-mysql_query('OPTIMIZE TABLE callnumber_browse;');
-mysql_query('OPTIMIZE TABLE callnumber_browse_metadata;');
-mysql_query('OPTIMIZE TABLE callnumber_browse_scoped_results_global;');
+optimizeTable('title_browse');
+optimizeTable('title_browse_metadata');
+optimizeTable('title_browse_scoped_results_global');
+optimizeTable('author_browse');
+optimizeTable('author_browse_metadata');
+optimizeTable('author_browse_scoped_results_global');
+optimizeTable('subject_browse');
+optimizeTable('subject_browse_metadata');
+optimizeTable('subject_browse_scoped_results_global');
+optimizeTable('callnumber_browse');
+optimizeTable('callnumber_browse_metadata');
+optimizeTable('callnumber_browse_scoped_results_global');
 
 while ($library->fetch()){
-	set_time_limit(300);
-	mysql_query("OPTIMIZE TABLE title_browse_scoped_results_library_{$library->subdomain};");
-	mysql_query("OPTIMIZE TABLE author_browse_scoped_results_library_{$library->subdomain};");
-	mysql_query("OPTIMIZE TABLE subject_browse_scoped_results_library_{$library->subdomain};");
-	mysql_query("OPTIMIZE TABLE callnumber_browse_scoped_results_library_{$library->subdomain};");
-	echo("<br>optimized browse tables for {$library->displayName}\r\n");
+	optimizeTable("title_browse_scoped_results_library_{$library->subdomain}");
+	optimizeTable("author_browse_scoped_results_library_{$library->subdomain}");
+	optimizeTable("subject_browse_scoped_results_library_{$library->subdomain}");
+	optimizeTable("callnumber_browse_scoped_results_library_{$library->subdomain}");
 }
 $logger->log('Finished optimizing tables', PEAR_LOG_INFO);
+
+function optimizeTable($tableName){
+	global $logger;
+	set_time_limit(1000);
+	echo("Optimizing $tableName<br/>\r\n");
+	mysql_query("OPTIMIZE TABLE $tableName;");
+	$logger->log('Optimized table: ' . $tableName, PEAR_LOG_INFO);
+}
