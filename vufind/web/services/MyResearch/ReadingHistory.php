a997d34f5ec1e0146013472792914fd896d66adf
Mark Noble <mnoble@turningleaftech.com>
2012-11-19 14:21:41 -0700

    When viewing reading history, the sort defaults to checkout date, but title is shown.  Fix so checkout date is defaulted.

--- vufind/web/services/MyResearch/ReadingHistory.php
+++ vufind/web/services/MyResearch/ReadingHistory.php
@@ -66,6 +66,7 @@ class ReadingHistory extends MyResearch
 					                     'checkedOut' => 'Checkout Date',
 					                     'format' => 'Format',
 					);
+					$selectedSortOption = isset($_REQUEST['accountSort']) ? $_REQUEST['accountSort'] : 'checkedOut';
 				}else{
 					$sortOptions = array('title' => 'Title',
 					                     'author' => 'Author',
@@ -73,9 +74,10 @@ class ReadingHistory extends MyResearch
 					                     'returned' => 'Return Date',
 					                     'format' => 'Format',
 					);
+					$selectedSortOption = isset($_REQUEST['accountSort']) ? $_REQUEST['accountSort'] : 'returned';
 				}
 				$interface->assign('sortOptions', $sortOptions);
-				$selectedSortOption = isset($_REQUEST['accountSort']) ? $_REQUEST['accountSort'] : 'returned';
+
 				$interface->assign('defaultSortOption', $selectedSortOption);
 				$page = isset($_REQUEST['page']) ? $_REQUEST['page'] : 1;
 
