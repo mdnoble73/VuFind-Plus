e87baa27643d576d1520baa0f73ec6ca32d4e052
Mark Noble <mark@marmot.org>
2013-04-02 08:57:45 -0600

    Make sure that all copies are loaded if we don't know what PType the user is.

--- vufind/web/Drivers/marmot_inc/MillenniumStatusLoader.php
+++ vufind/web/Drivers/marmot_inc/MillenniumStatusLoader.php
@@ -199,7 +199,7 @@ class MillenniumStatusLoader{
 
 			//Check to see if this item can be held by the current patron.  Only important when
 			//we know what pType is in use and we are showing all items.
-			if ($scope == 93 && $pType != -1){
+			if ($scope == 93 && $pType > 0){
 				if (!$driver->isItemHoldableToPatron($holding['locationCode'], $holding['iType'], $pType)){
 					//$logger->log("Removing item $holdingKey because it is not usable by the current patronType $pType, iType is {$holding['iType']}, location is {$holding['locationCode']}", PEAR_LOG_DEBUG);
 					unset($ret[$holdingKey]);
