b935421ec7987908b40f1fec52a71075c3bdd10c
Mark Noble <mark@marmot.org>
2013-08-26 16:44:22 -0600

    Optimizations for inventory app

--- vufind/web/services/Circa/AJAX.php
+++ vufind/web/services/Circa/AJAX.php
@@ -24,25 +24,25 @@ class Circa_AJAX extends Action{
 
 	function UpdateInventoryForBarcode(){
 		global $configArray;
-		$barcode = $_REQUEST['barcode'];
+		$barcode = is_array($_REQUEST['barcodes']) ? $_REQUEST['barcodes'] : array($_REQUEST['barcodes']);
 		$login = $_REQUEST['login'];
 		$password1 = $_REQUEST['password'];
 		$initials = $_REQUEST['initials'];
 		$password2 = $_REQUEST['password2'];
 		$updateIncorrectStatuses = $_REQUEST['updateIncorrectStatuses'];
 		$result = array(
-			'barcode' => $barcode
+			'barcode' => $barcode[0]
 		);
 
 		try {
 			$catalog = new CatalogConnection($configArray['Catalog']['driver']);
 			$results = $catalog->doInventory($login, $password1, $initials, $password2, $barcode, $updateIncorrectStatuses);
 			if ($results['success'] == false){
-				$result['needsAdditionalProcessing'] = true;
-				$result['inventoryResult'] = $results['message'];
+				$result['success'] = false;
+				$result['message'] = $results['message'];
 			}else{
-				$result['needsAdditionalProcessing'] = isset($results['barcodes'][$barcode]['needsAdditionalProcessing']) ? $results['barcodes'][$barcode]['needsAdditionalProcessing'] : false;
-				$result['inventoryResult'] = $results['barcodes'][$barcode]['inventoryResult'];
+				$result['success'] = true;
+				$result['barcodes'] = $results['barcodes'];
 			}
 		} catch (PDOException $e) {
 			// What should we do with this error?
