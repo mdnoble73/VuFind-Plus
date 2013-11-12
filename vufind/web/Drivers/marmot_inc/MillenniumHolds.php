c97814e9c4d140593e1d54f146a6df78cf315ccf
Mark Noble <mark@marmot.org>
2013-11-11 17:06:38 -0700

    Fix parsing of holds page for Sierra migration

--- vufind/web/Drivers/marmot_inc/MillenniumHolds.php
+++ vufind/web/Drivers/marmot_inc/MillenniumHolds.php
@@ -315,6 +315,9 @@ class MillenniumHolds{
 		$sCount = 0;
 		$sKeys = array_pad(array(),10,"");
 		foreach ($sRows as $sRow) {
+			if (strlen(trim($sRow)) == 0){
+				continue;
+			}
 			$sCols = preg_split("/<t(h|d)([^>]*)>/",$sRow);
 			$curHold= array();
 			$curHold['create'] = null;
@@ -348,8 +351,8 @@ class MillenniumHolds{
 					if (stripos($sKeys[$i],"TITLE") > -1) {
 						if (preg_match('/.*?<a href=\\"\/record=(.*?)(?:~S\\d{1,2})\\">(.*?)<\/a>.*/', $sCols[$i], $matches)) {
 							$shortId = $matches[1];
-							$bibid = '.' . $matches[1]; //Technically, this isn't corrcect since the check digit is missing
-							$title = $matches[2];
+							$bibid = '.' . $matches[1]; //Technically, this isn't correct since the check digit is missing
+							$title = strip_tags($matches[2]);
 						}else{
 							$bibid = '';
 							$shortId = '';
