c6ee8f66b8d7fef52f479e96a2512721544e7b4f
bmcfadden <benjamin.mcfadden@wakegov.com>
2013-12-19 13:20:54 -0500

    adding test popup tpl for "more" new books categories

--- vufind/web/services/Search/New_more.php
+++ vufind/web/services/Search/New_more.php
@@ -1,6 +1,8 @@
 <?php
 
 $interface->assign('popupTitle', 'New Books');
-//$popupContent = $interface->fetch('Search/new_more.tpl');
+$popupContent = $interface->fetch('Search/new_more.tpl');
 $interface->assign('popupContent', $popupContent);
 $interface->display('popup-wrapper.tpl');
+
+?>
