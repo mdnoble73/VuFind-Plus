b633adc0bfdb615771dce0bf5edea3b33743afc1
Mark Noble <mark@marmot.org>
2014-02-04 17:12:21 -0700

    Make Ratings data more generic Load Ratings using new Grouped Work API to allow the data to be accessed from multiple locations

--- vufind/web/RecordDrivers/GroupedWorkDriver.php
+++ vufind/web/RecordDrivers/GroupedWorkDriver.php
@@ -633,8 +633,8 @@ class GroupedWorkDriver implements RecordInterface{
 
 			// Loop through the ISBNs:
 			foreach($isbns as $isbn) {
-				// If we find an ISBN-10, return it immediately; otherwise, if we find
-				// an ISBN-13, save it if it is the first one encountered.
+				// If we find an ISBN-13, return it immediately; otherwise, if we find
+				// an ISBN-10, save it if it is the first one encountered.
 				$isbnObj = new ISBN($isbn);
 				if ($isbnObj->isValid()){
 					if ($isbn13  = $isbnObj->get13()) {
@@ -886,58 +886,8 @@ class GroupedWorkDriver implements RecordInterface{
 	}
 
 	public function getRatingData() {
-		global $user;
-		//Set default rating data
-		$ratingData = array(
-			'average' => 0,
-			'count'   => 0,
-			'user'    => 0,
-			'num1star' => 0,
-			'num2star' => 0,
-			'num3star' => 0,
-			'num4star' => 0,
-			'num5star' => 0,
-		);
-
-		require_once ROOT_DIR . '/sys/LocalEnrichment/UserWorkReview.php';
-		$reviewData = new UserWorkReview();
-		$reviewData->groupedRecordPermanentId = $this->getPermanentId();
-		$reviewData->find();
-		$totalRating = 0;
-		while ($reviewData->fetch()){
-			if ($reviewData->rating > 0){
-				$totalRating += $reviewData->rating;
-				$ratingData['count']++;
-				if ($user && $reviewData->userId == $user->id){
-					$ratingData['user'] = $reviewData->rating;
-				}
-				if ($reviewData->rating == 1){
-					$ratingData['num1star'] ++;
-				}elseif ($reviewData->rating == 2){
-					$ratingData['num2star'] ++;
-				}elseif ($reviewData->rating == 3){
-					$ratingData['num3star'] ++;
-				}elseif ($reviewData->rating == 4){
-					$ratingData['num4star'] ++;
-				}elseif ($reviewData->rating == 5){
-					$ratingData['num5star'] ++;
-				}
-			}
-		}
-		if ($ratingData['count'] > 0){
-			$ratingData['average'] = $totalRating / $ratingData['count'];
-			$ratingData['barWidth5Star'] = 100 * $ratingData['num5star'] / $ratingData['count'];
-			$ratingData['barWidth4Star'] = 100 * $ratingData['num4star'] / $ratingData['count'];
-			$ratingData['barWidth3Star'] = 100 * $ratingData['num3star'] / $ratingData['count'];
-			$ratingData['barWidth2Star'] = 100 * $ratingData['num2star'] / $ratingData['count'];
-			$ratingData['barWidth1Star'] = 100 * $ratingData['num1star'] / $ratingData['count'];
-		}else{
-			$ratingData['barWidth5Star'] = 0;
-			$ratingData['barWidth4Star'] = 0;
-			$ratingData['barWidth3Star'] = 0;
-			$ratingData['barWidth2Star'] = 0;
-			$ratingData['barWidth1Star'] = 0;
-		}
-		return $ratingData;
+		require_once ROOT_DIR . '/services/API/WorkAPI.php';
+		$workAPI = new WorkAPI();
+		return $workAPI->getRatingData($this->getPermanentId());
 	}
 }
