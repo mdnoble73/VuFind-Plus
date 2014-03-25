98b76320664117973827df3a75757dcf894e66e4
bmcfadden <benjamin.mcfadden@wakegov.com>
2014-03-25 10:52:06 -0400

    adding folder and blank file for responsive.js

--- vufind/web/interface/themes/responsive/js/vufind/responsive.js
+++ vufind/web/interface/themes/responsive/js/vufind/responsive.js
@@ -1,3 +1,37 @@
 /**
- * Created by bmcfadden on 3/25/14.
+ * Created by MNoble; copied over by bmcfadden  on 3/25/14.
  */
+
+VuFind.Responsive = (function(){
+    $(document).ready(function(){
+        $(window).resize(VuFind.Responsive.adjustLayout);
+        $(window).trigger('resize');
+    });
+
+    return {
+        adjustLayout: function(){
+            // get resolution
+            var resolution = document.documentElement.clientWidth;
+
+            var mainContentElement = $("#main-content-with-sidebar");
+            var xsContentInsertionPointElement = $("#xs-main-content-insertion-point");
+            var mainContent;
+            if (resolution < 750) {
+                // XS screen resolution
+                //move content from main-content-with-sidebar to xs-main-content-insertion-point
+                mainContent = mainContentElement.html();
+                if (mainContent && mainContent.length){
+                    xsContentInsertionPointElement.html(mainContent);
+                    mainContentElement.html("");
+                }
+            }else{
+                //Sm or better resolution
+                mainContent = xsContentInsertionPointElement.html();
+                if (mainContent && mainContent.length){
+                    mainContentElement.html(mainContent);
+                    xsContentInsertionPointElement.html("");
+                }
+            }
+        }
+    };
+}(VuFind.Responsive || {}));
