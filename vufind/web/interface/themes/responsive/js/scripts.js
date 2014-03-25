24322f575064db8cdcfc5d451d2e1ae1277debce
bmcfadden <benjamin.mcfadden@wakegov.com>
2014-03-25 15:51:13 -0400

    attempt 2: use responsive.js code copied into scripts.js and css to change page layout under 768 width

--- vufind/web/interface/themes/responsive/js/scripts.js
+++ vufind/web/interface/themes/responsive/js/scripts.js
@@ -1543,3 +1543,40 @@ $(document).ready(function(){
 	}
 });
 
+/**
+ * Created by MNoble; copied over by bmcfadden; then copied into this file from its own file (js/vufind/responsive.js  on 3/25/14.
+ */
+
+VuFind.Responsive = (function(){
+    $(document).ready(function(){
+        $(window).resize(VuFind.Responsive.adjustLayout);
+        $(window).trigger('resize');
+    });
+
+    return {
+        adjustLayout: function(){
+            // get resolution
+            var resolution = document.documentElement.clientWidth;
+
+            var mainContentElement = $("#main-content-with-sidebar");
+            var xsContentInsertionPointElement = $("#xs-main-content-insertion-point");
+            var mainContent;
+            if (resolution < 750) {
+                // XS screen resolution
+                //move content from main-content-with-sidebar to xs-main-content-insertion-point
+                mainContent = mainContentElement.html();
+                if (mainContent && mainContent.length){
+                    xsContentInsertionPointElement.html(mainContent);
+                    mainContentElement.html("");
+                }
+            }else{
+                //Sm or better resolution
+                mainContent = xsContentInsertionPointElement.html();
+                if (mainContent && mainContent.length){
+                    mainContentElement.html(mainContent);
+                    xsContentInsertionPointElement.html("");
+                }
+            }
+        }
+    };
+}(VuFind.Responsive || {}));
