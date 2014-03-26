d271fc85b0efe75028d5c82c223d7e679c88da07
bmcfadden <benjamin.mcfadden@wakegov.com>
2014-03-26 16:03:48 -0400

    testing a scroll down to bottom button thing

--- vufind/web/interface/themes/responsive/js/scripts.js
+++ vufind/web/interface/themes/responsive/js/scripts.js
@@ -1580,3 +1580,7 @@ VuFind.Responsive = (function(){
         }
     };
 }(VuFind.Responsive || {}));
+
+$(document).ready(function(){
+    $("<div style='position:fixed;top:50px;left:20px;background:#000;'>Scroll down and stuff</div>").appendTo("body");
+});
