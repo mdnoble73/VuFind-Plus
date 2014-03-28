9180998ce73ccab9f05c2d289ced35a94b26f9e5
Mark Noble <mark@marmot.org>
2014-03-28 10:48:54 -0600

    Fix hold cancellation date when placing holds on multiple titles

--- vufind/web/services/MyResearch/HoldMultiple.php
+++ vufind/web/services/MyResearch/HoldMultiple.php
@@ -189,8 +189,8 @@ class MyResearch_HoldMultiple extends Action
 					$interface->assign('focusElementId', 'username');
 				}
 
-				global $librarySingleton;
-				$patronHomeBranch = $librarySingleton->getPatronHomeLibrary();
+				global $library;
+				$patronHomeBranch = Library::getPatronHomeLibrary();
 				if ($patronHomeBranch != null){
 					if ($patronHomeBranch->defaultNotNeededAfterDays > 0){
 						$interface->assign('defaultNotNeededAfterDays', date('m/d/Y', time() + $patronHomeBranch->defaultNotNeededAfterDays * 60 * 60 * 24));
@@ -199,11 +199,21 @@ class MyResearch_HoldMultiple extends Action
 					}
 					$interface->assign('showHoldCancelDate', $patronHomeBranch->showHoldCancelDate);
 				}else{
-					//Show the hold cancellation date for now.  It may be hidden later when the user logs in.
-					$interface->assign('showHoldCancelDate', 1);
-					$interface->assign('defaultNotNeededAfterDays', '');
+					if ($library){
+						//Show the hold cancellation date for now.  It may be hidden later when the user logs in.
+						if ($library->defaultNotNeededAfterDays > 0){
+							$interface->assign('defaultNotNeededAfterDays', date('m/d/Y', time() + $library->defaultNotNeededAfterDays * 60 * 60 * 24));
+						}else{
+							$interface->assign('defaultNotNeededAfterDays', '');
+						}
+						$interface->assign('showHoldCancelDate', $library->showHoldCancelDate);
+					}else{
+						//Show the hold cancellation date for now.  It may be hidden later when the user logs in.
+						$interface->assign('showHoldCancelDate', 1);
+						$interface->assign('defaultNotNeededAfterDays', '');
+					}
 				}
-				$activeLibrary = $librarySingleton->getActiveLibrary();
+				$activeLibrary = Library::getActiveLibrary();
 				if ($activeLibrary != null){
 					$interface->assign('holdDisclaimer', $activeLibrary->holdDisclaimer);
 				}else{
