6b40b6edef3656a6b0496eb00bf5576eb1f9f666
pbrammeier <pascal@marmot.org>
2014-09-08 14:03:39 -0600

    Revert "Revert "Many Contributors shortened to display only 5 with show more/show less links.""
    This reverts commit e81c61ca07dc4bd2653900722d961bd1db838f93.
    
    Conflicts:
    	vufind/web/interface/themes/responsive/js/vufind.min.js

--- vufind/web/interface/themes/responsive/js/vufind/record.js
+++ vufind/web/interface/themes/responsive/js/vufind/record.js
@@ -390,6 +390,17 @@ VuFind.Record = (function(){
 					VuFind.showMessage('Hold Failed', data.message);
 				}
 			});
+		},
+
+		moreContributors: function(){
+			document.getElementById('showAdditionalContributorsLink').style.display="none";
+			document.getElementById('additionalContributors').style.display="block";
+		},
+
+		lessContributors: function(){
+			document.getElementById('showAdditionalContributorsLink').style.display="block";
+			document.getElementById('additionalContributors').style.display="none";
 		}
+
 	};
 }(VuFind.Record || {}));
