e6ae4ae8612e0f3ebd8260dd348b00f2b961cf44
Mark Noble <mark@marmot.org>
2014-10-21 11:35:19 -0600

    Continuous reindexing

--- sites/marmot.test/extract_and_reindex.sh
+++ sites/marmot.test/extract_and_reindex.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
-#export from sierra
-cd /usr/local/vufind-plus/vufind/sierra_export/; nice -n -10 java -jar sierra_export.jar marmot.test
-#export from overdrive
-cd /usr/local/vufind-plus/vufind/overdrive_api_extract/; nice -n -10 java -jar overdrive_extract.jar marmot.test
-#run reindex
-cd /usr/local/vufind-plus/vufind/reindexer; nice -n -5 java -jar reindexer.jar marmot.test
+while [true]; do
+	#export from sierra
+	cd /usr/local/vufind-plus/vufind/sierra_export/; nice -n -10 java -jar sierra_export.jar marmot.test
+	#export from overdrive
+	cd /usr/local/vufind-plus/vufind/overdrive_api_extract/; nice -n -10 java -jar overdrive_extract.jar marmot.test
+	#run reindex
+	cd /usr/local/vufind-plus/vufind/reindexer; nice -n -5 java -jar reindexer.jar marmot.test
+done
