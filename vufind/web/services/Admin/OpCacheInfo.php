59b7f80f78014f2846dd492bda4fcd9506bde3d3
pbrammeier <pascal@marmot.org>
2014-10-31 13:45:48 -0600

    VuFind Zend Opcache Integration
    addition of ZendOpcache Info page to the System Administration Tab.
    Also Modified PHPInfo to use a common template.

--- vufind/web/services/Admin/OpCacheInfo.php
+++ vufind/web/services/Admin/OpCacheInfo.php
@@ -1,33 +1,30 @@
 <?php
 /**
- * Returns information about PHP
+ * Created by PhpStorm.
+ * User: pbrammeier
+ * Date: 10/30/2014
+ * Time: 4:23 PM
  *
- * @category VuFind-Plus-2014 
- * @author Mark Noble <mark@marmot.org>
- * Date: 4/21/14
- * Time: 11:18 AM
- *
- * Modified 10-31-2014. plb
  */
 
 require_once ROOT_DIR . '/Action.php';
 require_once ROOT_DIR . '/services/Admin/Admin.php';
 
-class Admin_PHPInfo extends Admin_Admin {
+class Admin_OpCacheInfo extends Admin_Admin {
 	function launch() {
 		global $interface;
 
 		ob_start();
-		phpinfo();
+		include_once 'opcache-admin-include.php';
 		$info = ob_get_contents();
 		ob_end_clean();
 
 		$interface->assign("info", $info);
-		$interface->assign('title', 'PHP Information');
+		$interface->assign('title', 'Zend OpCache Information');
 
 		$interface->assign('sidebar', 'MyAccount/account-sidebar.tpl');
 		$interface->setTemplate('adminInfo.tpl');
-		$interface->setPageTitle('PHP Information');
+		$interface->setPageTitle('Zend OpCache Information');
 		$interface->display('layout.tpl');
 	}
 
