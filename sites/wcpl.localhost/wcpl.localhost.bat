94d03951905487cf0e245666e883c4926ff48f41
Mark Noble <mark@marmot.org>
2014-12-04 12:31:38 -0700

    Local Test System configuration - Nashville - Resolve Solr Port conflicts

--- sites/wcpl.localhost/wcpl.localhost.bat
+++ sites/wcpl.localhost/wcpl.localhost.bat
@@ -1,6 +1,6 @@
 @set VUFIND_HOME=c:\web\VuFind-Plus\sites\wcpl.localhost
 @set JETTY_HOME=c:\web\VuFind-Plus\sites\default\solr\jetty
 @set SOLR_HOME=c:\data\vufind-plus\wcpl.localhost\solr
-@set JETTY_PORT=8082
+@set JETTY_PORT=8084
 @set JETTY_LOG=c:\var\logs\vufind-plus\wcpl.localhost\logs\jetty
 @call ..\default\run_vufind.bat start %1 %2 %3 %4 %5 %6 %7 %8 %9
