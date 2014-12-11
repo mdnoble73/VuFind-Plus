ba3efe7f2c85bee85376341d844efcf016101e89
pbrammeier <pascal@marmot.org>
2014-12-11 12:10:46 -0700

    start-up updates continued

--- sites/default/pika_startup.sh
+++ sites/default/pika_startup.sh
@@ -1,14 +1,17 @@
 #!/bin/sh
 ### BEGIN INIT INFO
 # Provides: pika
-# Required-Start: mysql httpd memcached
+# Required-Start: mysqld httpd memcached
 # Default-Start: 2 3 4 5
 # Default-Stop: 0 1 6
-# Description: Pika init script. (formerly known as VuFind)  Change {servername} to your server name,
-# move the file to /etc/init.d/ , rename as pika, make executable,
-# and add service to startup options with "chkconfig pika on"
+# Description: Pika init script. (formerly known as VuFind)
+#   Change {servername} to your server name.
+#   Ensure the required-start daemons above match the daemon names on your server. use chkconfig --list
+#   Move the file to /etc/init.d/
+#   Rename as pika.sh, make executable.
+#   Add to startup sequence with "chkconfig pika on"
 ### END INIT INFO
 
-# Solr Engine for Marmot Test instance
+# Solr Engine for {servername} instance
 cd /usr/local/vufind-plus/sites/{servername}
 ./{servername}.sh start
