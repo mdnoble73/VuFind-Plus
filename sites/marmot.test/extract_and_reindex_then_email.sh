309a676285b4dab51ad2d3bba8e48ba0187e4696
pbrammeier <pascal@marmot.org>
2014-12-12 10:07:14 -0700

    extract & reindex then email script
    about to use on test server

--- sites/marmot.test/extract_and_reindex_then_email.sh
+++ sites/marmot.test/extract_and_reindex_then_email.sh
@@ -1,29 +1,8 @@
 #!/bin/bash
 # Script executes continuous re-indexing.
 #
-
-EMAIL=root@venus
-
-# this version should email output of each script when it finishes.
-while true
-do
-	echo "Starting new extract and index - `date`"
-	#export from sierra
-	cd /usr/local/vufind-plus/vufind/sierra_export/;
-	nice -n -10 java -jar sierra_export.jar marmot.test | mail -s "java -jar sierra_export.jar marmot.test" $EMAIL
-	#export from overdrive
-	cd /usr/local/vufind-plus/vufind/overdrive_api_extract/;
-	nice -n -10 java -jar overdrive_extract.jar marmot.test | mail -s "java -jar overdrive_extract.jar marmot.test" $EMAIL
-	#run reindex
-	cd /usr/local/vufind-plus/vufind/reindexer; nice -n -5 java -jar reindexer.jar marmot.test | mail -s "java -jar reindexer.jar marmot.test" $EMAIL
-done
-
-
-
-#!/bin/bash
-# Script executes continuous re-indexing.
-#
 # this version emails script output as a round finishes
+EMAIL=root@venus
 
 OUTPUT_FILE="extract_and_reindex_output.log"
 while true
@@ -54,3 +33,25 @@ do
 		mail -s "Extract and Reindexing - marmot.test" $EMAIL < $OUTPUT_FILE
 	fi
 done
+
+#!/bin/bash
+# Script executes continuous re-indexing.
+#
+EMAIL=root@venus
+
+
+# this version should email output of each script when it finishes.
+while true
+do
+	echo "Starting new extract and index - `date`"
+	#export from sierra
+	cd /usr/local/vufind-plus/vufind/sierra_export/;
+	nice -n -10 java -jar sierra_export.jar marmot.test | mail -s "java -jar sierra_export.jar marmot.test" $EMAIL
+	#export from overdrive
+	cd /usr/local/vufind-plus/vufind/overdrive_api_extract/;
+	nice -n -10 java -jar overdrive_extract.jar marmot.test | mail -s "java -jar overdrive_extract.jar marmot.test" $EMAIL
+	#run reindex
+	cd /usr/local/vufind-plus/vufind/reindexer; nice -n -5 java -jar reindexer.jar marmot.test | mail -s "java -jar reindexer.jar marmot.test" $EMAIL
+done
+
+
