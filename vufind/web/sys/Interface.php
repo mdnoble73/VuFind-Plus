e904a4ef3397400b90570b3443b2a3c0dd1304fe
pbrammeier <pascal@marmot.org>
2015-02-09 13:23:42 -0700

    use respond.js for ie9 compability modes.
    https://www.pivotaltracker.com/story/show/87691892

--- vufind/web/sys/Interface.php
+++ vufind/web/sys/Interface.php
@@ -157,7 +157,7 @@ class UInterface extends Smarty
 
 // Detect Internet Explorer 8 to include respond.js for responsive css support
 		if (isset($_SERVER['HTTP_USER_AGENT'])) {
-			$ie8 = stristr($_SERVER['HTTP_USER_AGENT'], 'msie 8');
+			$ie8 = stristr($_SERVER['HTTP_USER_AGENT'], 'msie 8') || stristr($_SERVER['HTTP_USER_AGENT'], 'trident/5'); //trident/5 should catch ie9 compability modes
 			$this->assign('ie8', $ie8);
 		}
 
