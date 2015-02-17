7078f84707cba1f3046a36f9de38e2f641d5cab6
Mark Noble <mark@marmot.org>
2015-02-17 09:26:39 -0700

    Create routine to process export from Millennium for continuous reindex

---
+++ sites/catalog.library.nashville.org/extract_and_reindex_then_email.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+# Script executes continuous re-indexing.
+#
+# this version emails script output as a round finishes
+EMAIL=James.Staub@nashville.gov;mark@marmot.org;pascal@marmot.org
+PIKASERVER=catalog.library.nashville.org
+ILSSERVER=waldo.library.nashville.org
+OUTPUT_FILE="/var/log/vufind-plus/${PIKASERVER}/extract_and_reindex_output.log"
+
+while true
+do
+        #truncate the file
+        : > $OUTPUT_FILE;
+
+        #echo "Starting new extract and index - `date`" > ${OUTPUT_FILE}
+        # reset the output file each round
+
+        #run expect script to extract from Millennium
+        cd /usr/local/vufind-plus/vufind/millennium_export/
+        ./ITEM_UPDATE_EXTRACT_PIKA.exp ${PIKASERVER} ${ILSSERVER} >> ${OUTPUT_FILE}
+
+        #process the export from Millennium to give Pika what it needs
+        #echo "Starting Millennium Export - `date`" >> ${OUTPUT_FILE}
+        cd /usr/local/vufind-plus/vufind/millennium_export/
+        nice -n -10 java -jar millennium_export.jar ${PIKASERVER} >> ${OUTPUT_FILE}
+
+        #export from overdrive
+        #echo "Starting OverDrive Extract - `date`" >> ${OUTPUT_FILE}
+        cd /usr/local/vufind-plus/vufind/overdrive_api_extract/
+        nice -n -10 java -jar overdrive_extract.jar ${PIKASERVER} >> ${OUTPUT_FILE}
+
+        #run reindex
+        #echo "Starting Reindexing - `date`" >> ${OUTPUT_FILE}
+        cd /usr/local/vufind-plus/vufind/reindexer
+        nice -n -5 java -jar reindexer.jar ${PIKASERVER} >> ${OUTPUT_FILE}
+
+        # add any logic wanted for when to send the emails here. (eg errors only)
+        FILESIZE=$(stat -c%s ${OUTPUT_FILE})
+        if [[ ${FILESIZE} > 0 ]]
+        then
+                # send mail
+                mail -s "Extract and Reindexing - ${PIKASERVER}" $EMAIL < ${OUTPUT_FILE}
+        fi
+done
