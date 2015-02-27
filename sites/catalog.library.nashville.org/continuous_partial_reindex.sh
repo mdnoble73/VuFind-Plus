e7aeb15dbe3f9f56c8e2e33086107eae4907c498
NashvillePublicLibrary <james.staub@nashville.gov>
2015-02-27 02:00:27 -0600

    84905464 : Nashville continuous partial reindex tweaks

--- sites/catalog.library.nashville.org/continuous_partial_reindex.sh
+++ sites/catalog.library.nashville.org/continuous_partial_reindex.sh
@@ -15,16 +15,20 @@ OUTPUT_FILE="/var/log/vufind-plus/${PIKASERVER}/continuous_partial_reindex_outpu
 
 # Check for conflicting processes currently runnin
 function checkConflictingProcesses() {
-	#echo $1
+	#Check to see if the conflict exists.
 	countConflictingProcesses=$(ps aux | grep -c $1)
 	countConflictingProcesses=$((countConflictingProcesses-1))
+	let numInitialConflicts=countConflictingProcesses
 	#echo "Count of conflicting process" $1 $countConflictingProcesses
+	#Wait until the conflict is gone.
 	until ((countConflictingProcesses == 0)); do
 		countConflictingProcesses=$(ps aux | grep -c $1)
 		countConflictingProcesses=$((countConflictingProcesses-1))
 		#echo "Count of conflicting process" $1 $countConflictingProcesses
 		sleep 300
 	done
+	#Return the number of conflicts we found initially.
+	echo ${numInitialConflicts};
 }
 
 # Prohibited time ranges - for, e.g., ILS backup
@@ -35,64 +39,94 @@ function checkProhibitedTimes() {
 	NOW=$(date +%H:%M:%S)
 	NOW=$(date --date=$NOW +%s)
 
+	hasConflicts=0
 	if (( $start < $stop ))
 	then
 		if (( $NOW > $start && $NOW < $stop ))
 		then
 			#echo "Sleeping:" $(($stop - $NOW))
 			sleep $(($stop - $NOW))
+			hasConflicts = 1
 		fi
 	elif (( $start > $stop ))
 	then
 		if (( $NOW < $stop ))
 		then
 			sleep $(($stop - $NOW))
+			hasConflicts = 1
 		elif (( $NOW > $start ))
 		then
 			sleep $(($stop + 86400 - $NOW))
+			hasConflicts = 1
 		fi
 	fi
+	echo ${hasConflicts};
 }	
 
 while true 
 do
+
+	#####
+	# Check to make sure this is a good time to run.
+	#####
+
 	# Make sure we are not running a Full Record Group/Reindex process
-	checkConflictingProcesses "full_update.sh"
+	hasConflicts = $(checkConflictingProcesses "full_update.sh")
+	#If we did get a conflict, restart the loop to make sure that all tests run
+	if (($? != 0)); then
+		continue
+	fi
 
-	# Nashville Millennium backup
-	checkProhibitedTimes "22:55" "02:30"
+	hasConflicts = $(checkConflictingProcesses "BIB_EXTRACT_PIKA.exp")
+	#If we did get a conflict, restart the loop to make sure that all tests run
+	if (($? != 0)); then
+		continue
+	fi
 
-		#truncate the file
-		: > $OUTPUT_FILE;
+	# Do not run while the Millennium backup is running to prevent inconsistencies with MARC records
+	# backup starts at 11 pm and ends by 2:30 am
+	# JAMES COMMENTED OUT 20150226 thinking backup might be irrelevant
+	#hasConflicts = $(checkProhibitedTimes "23:00" "02:30")
+	#If we did get a conflict, restart the loop to make sure that all tests run
+	#if (($? != 0)); then
+	#	continue
+	#fi
 
-		#echo "Starting new extract and index - `date`" > ${OUTPUT_FILE}
-		# reset the output file each round
+	#####
+	# Start of the actual indexing code
+	#####
 
-		#run expect script to extract from Millennium
-		#do not log to the output file since it has lots of data in it.
-		cd /usr/local/vufind-plus/vufind/millennium_export/
-		./ITEM_UPDATE_EXTRACT_PIKA.exp ${PIKASERVER} ${ILSSERVER}
+	#truncate the file
+	: > $OUTPUT_FILE;
 
-		#process the export from Millennium to give Pika what it needs
-		#echo "Starting Millennium Export - `date`" >> ${OUTPUT_FILE}
-		cd /usr/local/vufind-plus/vufind/millennium_export/
-		nice -n -10 java -jar millennium_export.jar ${PIKASERVER} >> ${OUTPUT_FILE}
+	#echo "Starting new extract and index - `date`" > ${OUTPUT_FILE}
+	# reset the output file each round
 
-		#export from overdrive
-		#echo "Starting OverDrive Extract - `date`" >> ${OUTPUT_FILE}
-		cd /usr/local/vufind-plus/vufind/overdrive_api_extract/
-		nice -n -10 java -jar overdrive_extract.jar ${PIKASERVER} >> ${OUTPUT_FILE}
+	#run expect script to extract from Millennium
+	#do not log to the output file since it has lots of data in it.
+	cd /usr/local/vufind-plus/vufind/millennium_export/
+	./ITEM_UPDATE_EXTRACT_PIKA.exp ${PIKASERVER} ${ILSSERVER}
 
-		#run reindex
-		#echo "Starting Reindexing - `date`" >> ${OUTPUT_FILE}
-		cd /usr/local/vufind-plus/vufind/reindexer
-		nice -n -5 java -jar reindexer.jar ${PIKASERVER} >> ${OUTPUT_FILE}
+	#process the export from Millennium to give Pika what it needs
+	#echo "Starting Millennium Export - `date`" >> ${OUTPUT_FILE}
+	cd /usr/local/vufind-plus/vufind/millennium_export/
+	nice -n -10 java -jar millennium_export.jar ${PIKASERVER} >> ${OUTPUT_FILE}
 
-		# add any logic wanted for when to send the emails here. (eg errors only)
-		FILESIZE=$(stat -c%s ${OUTPUT_FILE})
-		if [[ ${FILESIZE} > 0 ]]
-		then
-				# send mail
-				mail -s "Extract and Reindexing - ${PIKASERVER}" $EMAIL < ${OUTPUT_FILE}
-		fi
+	#export from overdrive
+	#echo "Starting OverDrive Extract - `date`" >> ${OUTPUT_FILE}
+	cd /usr/local/vufind-plus/vufind/overdrive_api_extract/
+	nice -n -10 java -jar overdrive_extract.jar ${PIKASERVER} >> ${OUTPUT_FILE}
+
+	#run reindex
+	#echo "Starting Reindexing - `date`" >> ${OUTPUT_FILE}
+	cd /usr/local/vufind-plus/vufind/reindexer
+	nice -n -5 java -jar reindexer.jar ${PIKASERVER} >> ${OUTPUT_FILE}
+
+	# add any logic wanted for when to send the emails here. (eg errors only)
+	FILESIZE=$(stat -c%s ${OUTPUT_FILE})
+	if [[ ${FILESIZE} > 0 ]]
+	then
+			# send mail
+			mail -s "Extract and Reindexing - ${PIKASERVER}" $EMAIL < ${OUTPUT_FILE}
+	fi
 done
