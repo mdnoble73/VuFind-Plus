c19390137df6d5d30730bf139f338bafce9c4098
Mark Noble <mark@marmot.org>
2015-03-04 21:28:03 -0700

    Add continuous index and full update scripts for marmot production server

--- sites/opac.marmot.org/continuous_partial_reindex.sh
+++ sites/opac.marmot.org/continuous_partial_reindex.sh
@@ -1,15 +1,9 @@
 #!/bin/bash
-# Mark Noble, Marmot Library Network
-# James Staub, Nashville Public Library
-# 20150218
 # Script executes continuous re-indexing.
-# Millennium 1.6_3
-
-# CONFIGURATION
-# PLEASE SET CONFLICTING PROCESSES AND PROHIBITED TIMES IN FUNCTION CALLS IN SCRIPT MAIN DO LOOP
+#
 # this version emails script output as a round finishes
-EMAIL=root@venus
-PIKASERVER=marmot.test
+EMAIL=root@mercury
+PIKASERVER=opac.marmot.org
 OUTPUT_FILE="/var/log/vufind-plus/${PIKASERVER}/extract_and_reindex_output.log"
 
 # Check for conflicting processes currently running
@@ -70,7 +64,7 @@ do
 	#####
 
 	# Make sure we are not running a Full Record Group/Reindex process
-	hasConflicts=$(checkConflictingProcesses "full_update_marmot_test.sh")
+	hasConflicts=$(checkConflictingProcesses "full_update_marmot_prod.sh")
 	#If we did get a conflict, restart the loop to make sure that all tests run
 	if (($? != 0)); then
 		continue
@@ -78,7 +72,7 @@ do
 
 	# Do not run while the export from Sierra is running to prevent inconsistencies with MARC records
 	# export starts at 10 pm the file is copied to the FTP server at about 11:40
-	hasConflicts=$(checkProhibitedTimes "21:50" "23:40")
+	hasConflicts=$(checkProhibitedTimes "21:50" "23:59")
 	#If we did get a conflict, restart the loop to make sure that all tests run
 	if (($? != 0)); then
 		continue
@@ -93,29 +87,29 @@ do
 
 	#echo "Starting new extract and index - `date`" > ${OUTPUT_FILE}
 	# reset the output file each round
-
-	#export from sierra (items, holds, and orders)
+	
+	#export from sierra
 	#echo "Starting Sierra Export - `date`" >> ${OUTPUT_FILE}
-	cd /usr/local/vufind-plus/vufind/sierra_export/
+	cd /usr/local/vufind-plus/vufind/sierra_export/;
 	nice -n -10 java -jar sierra_export.jar ${PIKASERVER} >> ${OUTPUT_FILE}
-
+	
 	#export from overdrive
 	#echo "Starting OverDrive Extract - `date`" >> ${OUTPUT_FILE}
-	cd /usr/local/vufind-plus/vufind/overdrive_api_extract/
+	cd /usr/local/vufind-plus/vufind/overdrive_api_extract/;
 	nice -n -10 java -jar overdrive_extract.jar ${PIKASERVER} >> ${OUTPUT_FILE}
-
+	
 	#run reindex
 	#echo "Starting Reindexing - `date`" >> ${OUTPUT_FILE}
-	cd /usr/local/vufind-plus/vufind/reindexer
+	cd /usr/local/vufind-plus/vufind/reindexer;
 	nice -n -5 java -jar reindexer.jar ${PIKASERVER} >> ${OUTPUT_FILE}
 
 	# add any logic wanted for when to send the emails here. (eg errors only)
 	FILESIZE=$(stat -c%s ${OUTPUT_FILE})
 	if [[ ${FILESIZE} > 0 ]]
 	then
-			# send mail
-			mail -s "Continuous Extract and Reindexing - ${PIKASERVER}" $EMAIL < ${OUTPUT_FILE}
+		# send mail
+		mail -s "Continuous Extract and Reindexing - ${PIKASERVER}" $EMAIL < ${OUTPUT_FILE}
 	fi
-
-		#end block
 done
+
+
