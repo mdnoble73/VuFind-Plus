54d550d186b80a158dd9fa0342efdee59b509f9d
Mark Noble <mark@marmot.org>
2015-06-23 09:18:20 -0600

    Fix slowness of indexing ^ #PK-739 update to use G1 Garbage Collection

--- sites/anythink.production/anythink.production.sh
+++ sites/anythink.production/anythink.production.sh
@@ -36,7 +36,7 @@ export JETTY_RUN
 export JETTY_PID=$JETTY_RUN/anythink.production.pid
 
 #Max memory should be at least the size of all solr indexes combined. 
-export JAVA_OPTIONS="-server -Xms1024m -Xmx6144m -XX:+UseParallelGC -XX:NewRatio=5"
+export JAVA_OPTIONS="-server -Xms1024m -Xmx6144m -XX:+UseG1GC"
 export JETTY_LOG=/var/log/anythink.production/logs/jetty
 
 exec /usr/local/vufind-plus/sites/default/vufind.sh $1 $2
