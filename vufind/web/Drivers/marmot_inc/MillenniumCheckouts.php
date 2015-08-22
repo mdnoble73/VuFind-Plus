c82871db6739359e7c0540ad1107cd4ca6e33792
pbrammeier <pascal@marmot.org>
2015-08-21 19:46:59 -0600

    PK-944  Wrong count for renewal successes.
    http://marmot.myjetbrains.com/youtrack/issue/PK-944
    
    Millennium Renew All method did not count correctly the number of items
    renewed.  The search key did not match what was displayed in the screen
    scrape from classic. #PK-944

--- vufind/web/Drivers/marmot_inc/MillenniumCheckouts.php
+++ vufind/web/Drivers/marmot_inc/MillenniumCheckouts.php
@@ -217,48 +217,12 @@ class MillenniumCheckouts {
 		$curCheckedOut = $patronDump['CUR_CHKOUT'];
 
 		//Login to the patron's account
-//		$cookieJar = tempnam ("/tmp", "CURLCOOKIE");
-//
-//		$curl_url = $this->driver->getVendorOpacUrl() . "/patroninfo";
-//		$logger->log('Loading page ' . $curl_url, PEAR_LOG_INFO);
-//
-//		$curl_connection = curl_init($curl_url);
-//		curl_setopt($curl_connection, CURLOPT_CONNECTTIMEOUT, 30);
-//		curl_setopt($curl_connection, CURLOPT_USERAGENT,"Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1)");
-//		curl_setopt($curl_connection, CURLOPT_RETURNTRANSFER, true);
-//		curl_setopt($curl_connection, CURLOPT_SSL_VERIFYPEER, false);
-//		curl_setopt($curl_connection, CURLOPT_FOLLOWLOCATION, 1);
-//		curl_setopt($curl_connection, CURLOPT_UNRESTRICTED_AUTH, true);
-//		curl_setopt($curl_connection, CURLOPT_COOKIEJAR, $cookieJar );
-//		curl_setopt($curl_connection, CURLOPT_COOKIESESSION, false);
-//		curl_setopt($curl_connection, CURLOPT_POST, true);
-//		$post_data = $this->driver->_getLoginFormValues($patron);
-//		$post_string = http_build_query($post_data);
-//		curl_setopt($curl_connection, CURLOPT_POSTFIELDS, $post_string);
-//		$loginResult = curl_exec($curl_connection);
-//		//When a library uses Encore, the initial login does a redirect and requires additional parameters.
-//		if (preg_match('/<input type="hidden" name="lt" value="(.*?)" \/>/si', $loginResult, $loginMatches)) {
-//			//Get the lt value
-//			$lt = $loginMatches[1];
-//			//Login again
-//			$post_data['lt'] = $lt;
-//			$post_data['_eventId'] = 'submit';
-//
-//			$post_string = http_build_query($post_data);
-//			curl_setopt($curl_connection, CURLOPT_POSTFIELDS, $post_string);
-//			curl_exec($curl_connection);
-//		}
 		$driver->_curl_login($patron);
 
 		//Go to the items page
 		$scope = $driver->getDefaultScope();
-//		$curl_url = $driver->getVendorOpacUrl() . "/patroninfo~S{$scope}/" . $patronDump['RECORD_#'] ."/items";
-//		curl_setopt($curl_connection, CURLOPT_URL, $curl_url);
-//		curl_setopt($curl_connection, CURLOPT_HTTPGET, true);
-//		curl_exec($curl_connection);
-
 		$curl_url = $this->driver->getVendorOpacUrl() . "/patroninfo~S{$scope}/" . $patronDump['RECORD_#'] ."/items";
-		$checkedOutPageText = $driver->_curlGetPage($curl_url);
+		$checkedOutPageText = $driver->_curlGetPage($curl_url); // TODO Initial page load needed?
 
 		//Post renewal information
 		$renewAllPostVariables = array(
@@ -266,15 +230,8 @@ class MillenniumCheckouts {
 			'renewall' => 'YES',
 		);
 
-//		$renewItemParams = http_build_query($renewAllPostVariables);
-//		curl_setopt($curl_connection, CURLOPT_URL, $curl_url);
-//		curl_setopt($curl_connection, CURLOPT_POST, true);
-//		curl_setopt($curl_connection, CURLOPT_POSTFIELDS, $renewItemParams);
 		$checkedOutPageText = $driver->_curlPostPage($curl_url, $renewAllPostVariables);
 		//$logger->log("Result of Renew All\r\n" . $sresult, PEAR_LOG_INFO);
-
-//		curl_close($curl_connection);
-//		unlink($cookieJar);
 
 		//Clear the existing patron info and get new information.
 		$renew_result = array(
@@ -284,8 +241,9 @@ class MillenniumCheckouts {
 			'Unrenewed' => 0
 		);
 		$renew_result['Total'] = $curCheckedOut;
-		preg_match_all("/RENEWED successfully/si", $checkedOutPageText, $matches);
-		$numRenewals = count($matches[0]);
+
+		// pattern from marmot sierra :  <b>  RENEWED</b> 
+		$numRenewals = preg_match_all("/<b>\s*RENEWED<\/b>/si", $checkedOutPageText, $matches);
 		$renew_result['Renewed'] = $numRenewals;
 		$renew_result['Unrenewed'] = $renew_result['Total'] - $renew_result['Renewed'];
 		if ($renew_result['Unrenewed'] > 0) {
@@ -310,11 +268,11 @@ class MillenniumCheckouts {
 					foreach ($rowMatches as $row) {
 						$row = $row[1];
 
-							//Extract failure  message
+							//Extract failure message
 							if (preg_match('/<td align="left" class="patFuncStatus">.*?<em><font color="red">(.*?)<\/font><\/em>.*?<\/td>/s', $row, $statusMatches)){
 								$msg = ucfirst(strtolower(trim( $statusMatches[1])));
 
-								// Add Title to msg
+								// Add Title to message
 								$title = $this->extract_title_from_row($row);
 
 								$renew_result['message'][] = "Unable to renew $title: $msg.";
