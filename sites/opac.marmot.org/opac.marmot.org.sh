7ac5957d6a2c37b70592c5c67704af8a2a4b8dff
pbrammeier <pascal@marmot.org>
2015-08-28 08:24:50 -0600

    Increase Solr memory for Marmot production

--- sites/opac.marmot.org/opac.marmot.org.sh
+++ sites/opac.marmot.org/opac.marmot.org.sh
@@ -5,9 +5,9 @@ export VUFIND_HOME=/usr/local/vufind-plus/sites/opac.marmot.org
 export JETTY_HOME=/usr/local/vufind-plus/sites/default/solr/jetty
 export SOLR_HOME=/data/vufind-plus/opac.marmot.org/solr
 export JETTY_PORT=8080
-#Max memory should be at least he size of all solr indexes combined. 
+#Max memory should be at least the size of all solr indexes combined.
 #export JAVA_OPTIONS="-server -Xms2g -Xmx26g -XX:+UseParallelGC -XX:NewRatio=5"
-export JAVA_OPTIONS="-server -Xms2g -Xmx32g -XX:+UseG1GC"
+export JAVA_OPTIONS="-server -Xms2g -Xmx36g -XX:+UseG1GC"
 export JETTY_LOG=/var/log/vufind-plus/opac.marmot.org/jetty
 
 exec /usr/local/vufind-plus/sites/default/vufind.sh $1 $2
