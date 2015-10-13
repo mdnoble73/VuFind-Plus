94fb680e94ebb96ff502b9411f708ecf4c452102
Mark Noble <mark@marmot.org>
2015-10-13 10:19:25 -0600

    Fix error in Solr startup scripts #PK-1247

--- sites/aspencat.test/start.solr4.sh
+++ sites/aspencat.test/start.solr4.sh
@@ -2,4 +2,4 @@
 rm -f /data/vufind-plus/aspencat.test/solr/lib/*
 cp /usr/local/vufind-plus/data_dir_setup/solr/lib/*.* /data/vufind-plus/aspencat.test/solr/lib/
 
-../default/solr/bin/solr start -m 6g -p 8081 -s "/data/vufind-plus/aspencat.test/solr" -d "/usr/local/vufind-plus/sites/default/solr/server"
+../default/solr/bin/solr start -m 6g -p 8081 -s "/data/vufind-plus/aspencat.test/solr" -d "/usr/local/vufind-plus/sites/default/solr/jetty"
--- sites/flatirons.test/start.solr4.sh
+++ sites/flatirons.test/start.solr4.sh
@@ -2,4 +2,4 @@
 rm -f /data/vufind-plus/flatirons.test/solr/lib/*
 cp /usr/local/vufind-plus/data_dir_setup/solr/lib/*.* /data/vufind-plus/flatirons.test/solr/lib/
 
-../default/solr/bin/solr start -m 6g -p 8085 -s "/data/vufind-plus/flatirons.test/solr" -d "/usr/local/vufind-plus/sites/default/solr/server"
+../default/solr/bin/solr start -m 6g -p 8085 -s "/data/vufind-plus/flatirons.test/solr" -d "/usr/local/vufind-plus/sites/default/solr/jetty"
