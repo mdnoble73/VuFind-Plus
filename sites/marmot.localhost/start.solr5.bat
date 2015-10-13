3e610c95049fe9b82ea111b699563910c61e58bf
Mark Noble <mark@marmot.org>
2015-10-12 22:44:33 -0600

    Set memory for Solr 5 on local machines #PK-1206

--- sites/marmot.localhost/start.solr5.bat
+++ sites/marmot.localhost/start.solr5.bat
@@ -2,4 +2,4 @@ rem Start Solr
 rem @set SOLR_HOME=c:\data\vufind-plus\marmot.localhost\solr
 rem @set JETTY_HOME=c:\web\VuFind-Plus\sites\default\solr\jetty
 
-..\default\solr5\bin\solr.cmd start -p 8080 -s "c:\data\vufind-plus\marmot.localhost\solr" -d "c:\web\VuFind-Plus\sites\default\solr5\server"
+..\default\solr5\bin\solr.cmd start -p 8080 -m 2g -s "c:\data\vufind-plus\marmot.localhost\solr" -d "c:\web\VuFind-Plus\sites\default\solr5\server"
