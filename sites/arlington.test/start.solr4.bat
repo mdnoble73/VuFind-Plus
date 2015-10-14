4fd9722abeb6de69770ae870aea6733460f07f6c
Mark Noble <mark@marmot.org>
2015-10-14 13:48:54 -0600

    Fix arlington index start and stop scripts

--- sites/arlington.test/start.solr4.bat
+++
@@ -1,6 +0,0 @@
-rem Start Solr
-rem @set SOLR_HOME=c:\data\vufind-plus\marmot.localhost\solr
-rem @set JETTY_HOME=c:\web\VuFind-Plus\sites\default\solr\jetty
-rm -f c:\data\vufind-plus\arlington.localhost\solr\lib\*
-cp c:/web/VuFind-Plus/data_dir_setup/solr/lib/* c:/data/vufind-plus/arlington.localhost/solr/lib/
-..\default\solr\bin\solr.cmd start -p 8086 -m 2g -s "c:\data\vufind-plus\arlington.localhost\solr" -d "c:\web\VuFind-Plus\sites\default\solr\jetty"
---
+++ sites/arlington.test/stop.solr4.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+../default/solr/bin/solr stop -p 8086 -d "/usr/local/vufind-plus/sites/default/solr/jetty"
