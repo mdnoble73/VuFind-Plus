dc485d92d4f763529b3ed02bf98367f64a5aef92
Mark Noble <mark@marmot.org>
2015-10-16 16:25:21 -0600

    Fix syntax errors in Solr 4 startup scripts

--- sites/flatirons.test/flatirons.test.sh
+++ sites/flatirons.test/flatirons.test.sh
@@ -5,12 +5,12 @@ if [ -z "$1" ]
     echo "To use, run with start, stop or restart for the first parameter."
 fi
 
-if [ "$1" eq "stop" or "$1" eq "restart" ]
+if [[ ( "$1" == "stop" ) || ( "$1" == "restart") ]]
 	then
 		../default/solr/bin/solr stop -p 8085 -d "/usr/local/vufind-plus/sites/default/solr/jetty"
 fi
 
-if [ "$1" eq "start" or "$1" eq "restart" ]
+if [[ ( "$1" == "start" ) || ( "$1" == "restart") ]]
 	then
 		../default/solr/bin/solr start -m 4g -p 8085 -s "/data/vufind-plus/flatirons.test/solr" -d "/usr/local/vufind-plus/sites/default/solr/jetty"
 fi
--- sites/testvufind.co.wake.nc.us/testvufind.co.wake.nc.us.sh
+++ sites/testvufind.co.wake.nc.us/testvufind.co.wake.nc.us.sh
@@ -5,12 +5,12 @@ if [ -z "$1" ]
     echo "To use, run with start, stop or restart for the first parameter."
 fi
 
-if [ "$1" eq "stop" or "$1" eq "restart" ]
+if [[ ( "$1" == "stop" ) || ( "$1" == "restart") ]]
 	then
 		../default/solr/bin/solr stop -p 8080 -d "/usr/local/vufind-plus/sites/default/solr/jetty"
 fi
 
-if [ "$1" eq "start" or "$1" eq "restart" ]
+if [[ ( "$1" == "start" ) || ( "$1" == "restart") ]]
 	then
 		../default/solr/bin/solr start -m 8g -p 8080 -s "/data/vufind-plus/testvufind.co.wake.nc.us/solr" -d "/usr/local/vufind-plus/sites/default/solr/jetty"
 fi
