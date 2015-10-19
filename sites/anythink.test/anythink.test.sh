0bbb8d89f585926f3ebcb4da06ae552e1dba2de7
Mark Noble <mark@marmot.org>
2015-10-19 16:25:31 -0600

    Fix Anythink Solr start/stop script

--- sites/anythink.test/anythink.test.sh
+++ sites/anythink.test/anythink.test.sh
@@ -7,7 +7,7 @@ fi
 
 if [[ ( "$1" == "stop" ) || ( "$1" == "restart") ]]
 	then
-		../default/solr/bin/solr stop -p 8080 -d "/usr/local/vufind-plus/sites/default/solr/jetty"
+		../default/solr/bin/solr stop -p 8082 -d "/usr/local/vufind-plus/sites/default/solr/jetty"
 fi
 
 if [[ ( "$1" == "start" ) || ( "$1" == "restart") ]]
