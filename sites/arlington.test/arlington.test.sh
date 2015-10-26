e55ee42fd70cd83428b3225435651fd936da6632
Mark Noble <mark@marmot.org>
2015-10-26 10:06:21 -0600

    Update memory for arlington and flatirons test servers

--- sites/arlington.test/arlington.test.sh
+++ sites/arlington.test/arlington.test.sh
@@ -14,5 +14,5 @@ fi
 if [[ ( "$1" == "start" ) || ( "$1" == "restart") ]]
 	then
 	../default/solr/bin/solr start -m 2g -p 8186 -s "/data/vufind-plus/arlington.test/solr_master" -d "/usr/local/vufind-plus/sites/default/solr/jetty"
-	../default/solr/bin/solr start -m 3g -p 8086 -a "-Dsolr.masterport=8186" -s "/data/vufind-plus/arlington.test/solr_searcher" -d "/usr/local/vufind-plus/sites/default/solr/jetty"
+	../default/solr/bin/solr start -m 2g -p 8086 -a "-Dsolr.masterport=8186" -s "/data/vufind-plus/arlington.test/solr_searcher" -d "/usr/local/vufind-plus/sites/default/solr/jetty"
 fi
--- sites/flatirons.test/flatirons.test.sh
+++ sites/flatirons.test/flatirons.test.sh
@@ -14,5 +14,5 @@ fi
 if [[ ( "$1" == "start" ) || ( "$1" == "restart") ]]
 	then
 		../default/solr/bin/solr start -m 2g -p 8185 -s "/data/vufind-plus/flatirons.test/solr_master" -d "/usr/local/vufind-plus/sites/default/solr/jetty"
-		../default/solr/bin/solr start -m 2g -p 8085 -a "-Dsolr.masterport=8185" -s "/data/vufind-plus/flatirons.test/solr_searcher" -d "/usr/local/vufind-plus/sites/default/solr/jetty"
+		../default/solr/bin/solr start -m 3g -p 8085 -a "-Dsolr.masterport=8185" -s "/data/vufind-plus/flatirons.test/solr_searcher" -d "/usr/local/vufind-plus/sites/default/solr/jetty"
 fi
