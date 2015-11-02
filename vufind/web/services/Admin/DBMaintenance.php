e679741a7ded464521272f3ff52f80fe257d3a8f
Mark Noble <mark@marmot.org>
2015-11-02 14:48:24 -0700

    More eVoke cleanup

--- vufind/web/services/Admin/DBMaintenance.php
+++ vufind/web/services/Admin/DBMaintenance.php
@@ -89,15 +89,12 @@ class DBMaintenance extends Admin_Admin {
 		$user_updates = getUserUpdates();
 		require_once ROOT_DIR . '/sys/DBMaintenance/list_widget_updates.php';
 		$list_widget_updates = getListWidgetUpdates();
-		require_once ROOT_DIR . '/sys/DBMaintenance/evoke_updates.php';
-		$evoke_updates = getEVokeUpdates();
 
 		return array_merge(
 			$library_location_updates,
 			$user_updates,
 			$grouped_work_updates,
 			$list_widget_updates,
-			$evoke_updates,
 			array(
 				'index_search_stats' => array(
 					'title' => 'Index search stats table',
