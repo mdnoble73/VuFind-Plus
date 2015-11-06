1079fccd4bb5ccc580f8dedbdde3a52ef56e18f0
Mark Noble <mark@marmot.org>
2015-11-05 21:26:14 -0700

    Correct startup script for aspencat production

--- sites/aspencat.production/aspencat.production.sh
+++ sites/aspencat.production/aspencat.production.sh
@@ -13,6 +13,6 @@ fi
 
 if [[ ( "$1" == "start" ) || ( "$1" == "restart") ]]
 	then
-		../default/solr/bin/solr start -m 4g -p 8181 -s "/data/vufind-plus/arlington.test/solr_master" -d "/usr/local/vufind-plus/sites/default/solr/jetty"
-		../default/solr/bin/solr start -m 6g -p 8081 -a "-Dsolr.masterport=8181" -s "/data/vufind-plus/arlington.test/solr_searcher" -d "/usr/local/vufind-plus/sites/default/solr/jetty"
+		../default/solr/bin/solr start -m 4g -p 8181 -s "/data/vufind-plus/aspencat.production/solr_master" -d "/usr/local/vufind-plus/sites/default/solr/jetty"
+		../default/solr/bin/solr start -m 6g -p 8081 -a "-Dsolr.masterport=8181" -s "/data/vufind-plus/aspencat.production/solr_searcher" -d "/usr/local/vufind-plus/sites/default/solr/jetty"
 fi
