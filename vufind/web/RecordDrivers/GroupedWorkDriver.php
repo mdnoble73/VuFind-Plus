4ad97e1a630266f1e8d91cce2c8578377c5e7511
Mark Noble <mark@marmot.org>
2015-11-16 17:28:37 -0700

    Fix commenting out BibFrame #PK-1365

--- vufind/web/RecordDrivers/GroupedWorkDriver.php
+++ vufind/web/RecordDrivers/GroupedWorkDriver.php
@@ -2287,12 +2287,12 @@ class GroupedWorkDriver extends RecordInterface{
 				'author' => $this->getPrimaryAuthor(),
 		);
 		//BibFrame
-		/*$semanticData[] = array(
-				'@context' => 'http://bibfra.me/view/lite/',
-				'@type' => 'Work', /* TODO: This should change to a more specific type Book/Movie as applicable */
-				'title' => $this->getTitle(),
-				'author' => $this->getPrimaryAuthor(),
-		);*/
+		//$semanticData[] = array(
+		//		'@context' => 'http://bibfra.me/view/lite/',
+		//		'@type' => 'Work', /* TODO: This should change to a more specific type Book/Movie as applicable */
+		//		'title' => $this->getTitle(),
+		//		'author' => $this->getPrimaryAuthor(),
+		//);
 		//TODO: add audience, award, content
 		return $semanticData;
 	}
