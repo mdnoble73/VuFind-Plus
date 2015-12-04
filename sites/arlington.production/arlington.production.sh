c8408efcecdc6f466bd8ed4014d8af59b071dce0
pbrammeier <pascal@marmot.org>
2015-12-04 16:17:37 -0700

    ARL-44 Arlington Production Settings
    http://marmot.myjetbrains.com/youtrack/issue/ARL-44
    
    Initial Arlington Production Settings #ARL-44

---
+++ sites/arlington.production/arlington.production.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+if [ -z "$1" ]
+  then
+    echo "To use, run with start, stop or restart for the first parameter."
+fi
+
+if [[ ( "$1" == "stop" ) || ( "$1" == "restart") ]]
+	then
+		../default/solr/bin/solr stop -p 8186 -d "/usr/local/vufind-plus/sites/default/solr/jetty"
+		../default/solr/bin/solr stop -p 8086 -d "/usr/local/vufind-plus/sites/default/solr/jetty"
+fi
+
+if [[ ( "$1" == "start" ) || ( "$1" == "restart") ]]
+	then
+	../default/solr/bin/solr start -m 4g -p 8186 -s "/data/vufind-plus/arlington.production/solr_master" -d "/usr/local/vufind-plus/sites/default/solr/jetty"
+	../default/solr/bin/solr start -m 4g -p 8086 -a "-Dsolr.masterport=8186" -s "/data/vufind-plus/arlington.production/solr_searcher" -d "/usr/local/vufind-plus/sites/default/solr/jetty"
+fi
