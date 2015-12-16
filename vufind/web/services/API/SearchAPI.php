943ae505f4861d1a90d4d9eaeee25831b1947397
pbrammeier <pascal@marmot.org>
2015-12-16 14:18:21 -0700

    Prevent php notice in Search API

--- vufind/web/services/API/SearchAPI.php
+++ vufind/web/services/API/SearchAPI.php
@@ -30,7 +30,7 @@ class SearchAPI extends Action {
 		header('Cache-Control: no-cache, must-revalidate'); // HTTP/1.1
 		header('Expires: Mon, 26 Jul 1997 05:00:00 GMT'); // Date in the past
 
-		if (is_callable(array($this, $_GET['method']))) {
+		if (!empty($_GET['method']) && is_callable(array($this, $_GET['method']))) {
 			if (in_array($_GET['method'] , array('getSearchBar', 'getListWidget'))){
 				$output = $this->$_GET['method']();
 			}else{
