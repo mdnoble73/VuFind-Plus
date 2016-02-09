85957f86a60aa1a96d9b5835f2583abc36b91c07
pbrammeier <pascal@marmot.org>
2016-02-09 11:36:46 -0700

    DR-108 Add Islandora Drivers
    http://marmot.myjetbrains.com/youtrack/issue/DR-108
    
    Audio & Newspaper Drivers. #DR-108

--- vufind/web/RecordDrivers/Factory.php
+++ vufind/web/RecordDrivers/Factory.php
@@ -44,10 +44,9 @@ class RecordDriverFactory {
 		if (is_array($record) && !array_key_exists('recordtype', $record)){
 			$recordType = $record['RELS_EXT_hasModel_uri_s'];
 			//Get rid of islandora namespace information
-			$recordType = str_replace('info:fedora/islandora:', '', $recordType);
-			$recordType = str_replace('sp_', '', $recordType);
-			$recordType = str_replace('_cmodel', '', $recordType);
-			$recordType = str_replace('CModel', '', $recordType);
+			$recordType = str_replace(array(
+					'info:fedora/islandora:', 'sp_', 'sp-', '_cmodel', 'CModel',
+				), '', $recordType);
 			$driverNameParts = explode('_', $recordType);
 			$normalizedRecordType = '';
 			foreach ($driverNameParts as $driverPart){
