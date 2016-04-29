1fc060e55681a176a47967fef8382b557ec19071
Mark Noble <mark@marmot.org>
2016-04-29 09:42:28 -0600

    Additional null pointer checking in Record Grouping

--- vufind/record_grouping/src/org/vufind/RecordGrouperMain.java
+++ vufind/record_grouping/src/org/vufind/RecordGrouperMain.java
@@ -1321,7 +1321,12 @@ public class RecordGrouperMain {
 									RecordIdentifier recordIdentifier = recordGroupingProcessor.getPrimaryIdentifierFromMarcRecord(curBib, curProfile.name);
 									if (recordIdentifier == null) {
 										//logger.debug("Record with control number " + curBib.getControlNumber() + " was suppressed or is eContent");
-										suppressedControlNumbersInExport.add(curBib.getControlNumber());
+										String controlNumebr = curBib.getControlNumber();
+										if (controlNumebr != null) {
+											suppressedControlNumbersInExport.add(curBib.getControlNumber());
+										}else{
+											logger.warn("Bib did not have control number or identifier");
+										}
 									}else if (recordIdentifier.isSuppressed()) {
 										//logger.debug("Record with control number " + curBib.getControlNumber() + " was suppressed or is eContent");
 										suppressedControlNumbersInExport.add(recordIdentifier.getIdentifier());
