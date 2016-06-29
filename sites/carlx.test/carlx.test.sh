a88c682e1bf4305191646c8127d4240a82d27b8d
pbrammeier <pascal@marmot.org>
2016-06-29 15:10:34 -0600

    CarlX Test Server Set-up #PL-2056
    http://marmot.myjetbrains.com/youtrack/issue/PK-2056

--- sites/carlx.test/carlx.test.sh
+++ sites/carlx.test/carlx.test.sh
@@ -7,12 +7,12 @@ fi
 
 if [[ ( "$1" == "stop" ) || ( "$1" == "restart") ]]
 	then
-		../default/solr/bin/solr stop -p 8187 -d "/usr/local/vufind-plus/sites/default/solr/jetty"
-		../default/solr/bin/solr stop -p 8087 -d "/usr/local/vufind-plus/sites/default/solr/jetty"
+		../default/solr/bin/solr stop -p 8180 -d "/usr/local/vufind-plus/sites/default/solr/jetty"
+		../default/solr/bin/solr stop -p 8080 -d "/usr/local/vufind-plus/sites/default/solr/jetty"
 fi
 
 if [[ ( "$1" == "start" ) || ( "$1" == "restart") ]]
 	then
-		../default/solr/bin/solr start -m 9g -p 8187 -s "/data/vufind-plus/carlx.test/solr_master" -d "/usr/local/vufind-plus/sites/default/solr/jetty"
-		../default/solr/bin/solr start -m 15g -p 8087 -a "-Dsolr.masterport=8187" -s "/data/vufind-plus/carlx.test/solr_searcher" -d "/usr/local/vufind-plus/sites/default/solr/jetty"
+		../default/solr/bin/solr start -m 9g -p 8180 -s "/data/vufind-plus/carlx.test/solr_master" -d "/usr/local/vufind-plus/sites/default/solr/jetty"
+		../default/solr/bin/solr start -m 15g -p 8080 -a "-Dsolr.masterport=8180" -s "/data/vufind-plus/carlx.test/solr_searcher" -d "/usr/local/vufind-plus/sites/default/solr/jetty"
 fi
