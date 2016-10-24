a628d4b86ac674dde6cb90c0452c8652e5a37266
jabedo <jabedo@hotmail.com>
2016-10-24 06:59:53 -0600

    fix to broken php code

--- vufind/web/RecordDrivers/MarcRecord.php
+++ vufind/web/RecordDrivers/MarcRecord.php
@@ -2221,11 +2221,5 @@ class MarcRecord extends IndexRecord
 	     
 	}
 	
-	
-	abstract class BookFormatType{
-		 const  AudiobookFormat = 0;
-		 const EBook =1;
-		 const Hardcover =2;
-		 const Paperback =3;
 	}
 	
