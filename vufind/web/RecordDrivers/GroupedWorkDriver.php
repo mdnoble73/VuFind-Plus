e2fd59e993f0a9f8b81a4d8431215caafc688051
pbrammeier <pascal@marmot.org>
2017-01-11 14:52:20 -0700

    Fix Search Navigation on Record Views that aren't Grouped Works #PK-2767
    https://marmot.myjetbrains.com/youtrack/issue/PK-2767

--- vufind/web/RecordDrivers/GroupedWorkDriver.php
+++ vufind/web/RecordDrivers/GroupedWorkDriver.php
@@ -532,6 +532,7 @@ class GroupedWorkDriver extends RecordInterface{
 		if (count($relatedRecords) == 1){
 			$firstRecord = reset($relatedRecords);
 			$linkUrl = $firstRecord['url'];
+			$linkUrl .=  '?searchId=' . $interface->get_template_vars('searchId') . '&amp;recordIndex=' . $interface->get_template_vars('recordIndex') . '&amp;page='  . $interface->get_template_vars('page');
 		}else{
 			$linkUrl = '/GroupedWork/' . $id . '/Home?searchId=' . $interface->get_template_vars('searchId') . '&amp;recordIndex=' . $interface->get_template_vars('recordIndex') . '&amp;page='  . $interface->get_template_vars('page');
 			if ($useUnscopedHoldingsSummary){
