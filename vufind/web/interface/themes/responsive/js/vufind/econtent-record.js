7d0eff49d0935686983772218d6021980c666200
pbrammeier <pascal@marmot.org>
2017-04-25 14:03:46 -0600

    eContent support form continued #D-406
    https://marmot.myjetbrains.com/youtrack/issue/D-406

--- vufind/web/interface/themes/responsive/js/vufind/econtent-record.js
+++ vufind/web/interface/themes/responsive/js/vufind/econtent-record.js
@@ -10,22 +10,6 @@ VuFind.EContent = (function(){
 					},
 					'json').fail(function(){VuFind.ajaxFail()});
 			return false;
-		// submitHelpForm: function(){
-		// 	var url = Globals.path + '/Help/eContentSupport';
-		// 	$.ajax({
-		// 		type: "POST",
-		// 		url: url,
-		// 		data: $("#eContentSupport").serialize(), // serializes the form's elements.
-		// 		success: function(data){
-		// 			var jsonData = JSON.parse(data);
-		// 			VuFind.showMessage(jsonData.title, jsonData.message);
-		// 		},
-		// 		failure: function(data){
-		// 			alert("Could not submit the form");
-		// 		}
-		// 	});
-		//
-		// 	return false;
 		}
 	}
 }(VuFind.EContent));
