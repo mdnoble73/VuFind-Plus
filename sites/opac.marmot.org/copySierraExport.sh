4829493a7b92f9cf6b1de82b5f3bca729eb15092
pbrammeier <pascal@marmot.org>
2017-04-27 13:59:33 -0600

    only copy Marmot full export on ftp server if it is newer. #D-392
    https://marmot.myjetbrains.com/youtrack/issue/D-932

---
+++ sites/opac.marmot.org/copySierraExport.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+#Retrieve marc records from the FTP server
+mount 10.1.2.6:/ftp/sierra /mnt/ftp
+cp --update /mnt/ftp/fullexport.marc /data/vufind-plus/opac.marmot.org/marc/fullexport.mrc
+umount /mnt/ftp
+
