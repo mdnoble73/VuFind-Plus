2613aae5f980c01878f0358649f7eaaed0909caa
pbrammeier <pascal@marmot.org>
2017-04-28 13:45:40 -0600

    preserve timestamp of fullexport file from Sierra server. #D-933
    https://marmot.myjetbrains.com/youtrack/issue/D-933

--- sites/opac.marmot.org/copySierraExport.sh
+++ sites/opac.marmot.org/copySierraExport.sh
@@ -2,6 +2,6 @@
 
 #Retrieve marc records from the FTP server
 mount 10.1.2.6:/ftp/sierra /mnt/ftp
-cp --update /mnt/ftp/fullexport.marc /data/vufind-plus/opac.marmot.org/marc/fullexport.mrc
+cp --preserve=timestamps --update /mnt/ftp/fullexport.marc /data/vufind-plus/opac.marmot.org/marc/fullexport.mrc
 umount /mnt/ftp
 
