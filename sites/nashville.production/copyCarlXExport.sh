8fd95bcf05a32925cb977d66563b4cb897f1ffbb
Mark Noble <mark@marmot.org>
2017-05-31 14:41:21 -0600

    Work on copying files #D-987 & #D-832

---
+++ sites/nashville.production/copyCarlXExport.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+#Retrieve marc records from the FTP server
+mount 10.1.2.6:/ftp/sierra /mnt/ftp
+# ftp1.marmot.org
+#mount 10.1.2.7:/ftp/sierra /mnt/ftp
+# sftp.marmot.org server
+
+#copy production extract
+cp --preserve=timestamps --update /mnt/ftp/fullexport.marc /data/vufind-plus/marmot.test/marc/fullexport.mrc
+umount /mnt/ftp
+
