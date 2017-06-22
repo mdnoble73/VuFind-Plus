3f8cf655500da653d991f96a9b84c3331bcdc885
pbrammeier <pascal@marmot.org>
2017-06-22 16:53:48 -0600

    Sirsi Driver - show position in hold queue #D-1149
    https://marmot.myjetbrains.com/youtrack/issue/D-1149

--- vufind/web/services/MyAccount/Holds.php
+++ vufind/web/services/MyAccount/Holds.php
@@ -21,7 +21,7 @@ class MyAccount_Holds extends MyAccount{
 		$interface->assign('allowFreezeHolds', true);
 
 		$ils = $configArray['Catalog']['ils'];
-		$showPosition = ($ils == 'Horizon' || $ils == 'Koha');
+		$showPosition = ($ils == 'Horizon' || $ils == 'Koha' || $ils == 'Symphony');
 		$showExpireTime = ($ils == 'Horizon');
 		$suspendRequiresReactivationDate = ($ils == 'Horizon' || $ils == 'CarlX' || $ils == 'Symphony');
 		$interface->assign('suspendRequiresReactivationDate', $suspendRequiresReactivationDate);
