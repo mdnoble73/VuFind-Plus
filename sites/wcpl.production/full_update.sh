81dd23421c903b00c70330dd22c708ee2261d6bf
pbrammeier <pascal@marmot.org>
2017-07-03 10:44:38 -0600

    send full update emails to wake county #D-1155
    https://marmot.myjetbrains.com/youtrack/issue/D-1155

--- sites/wcpl.production/full_update.sh
+++ sites/wcpl.production/full_update.sh
@@ -9,7 +9,7 @@
 # For Pika discovery partners using Millennium 2011 1.6_3
 
 # this version emails script output as a round finishes
-EMAIL=root@amalthea.marmot.org
+EMAIL=root@amalthea.marmot.org,wake.pika@wakegov.com
 PIKASERVER=wcpl.production
 PIKADBNAME=pika
 OUTPUT_FILE="/var/log/vufind-plus/${PIKASERVER}/full_update_output.log"
