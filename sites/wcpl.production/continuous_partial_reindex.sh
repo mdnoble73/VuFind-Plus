bba181dfde27a9e67f231010e7c56d834e221a25
Mark Noble <mark@marmot.org>
2017-08-03 21:00:05 -0600

    update e-mail to e-mail changes for wcpl production

--- sites/wcpl.production/continuous_partial_reindex.sh
+++ sites/wcpl.production/continuous_partial_reindex.sh
@@ -8,7 +8,7 @@
 # CONFIGURATION
 # PLEASE SET CONFLICTING PROCESSES AND PROHIBITED TIMES IN FUNCTION CALLS IN SCRIPT MAIN DO LOOP
 # this version emails script output as a round finishes
-EMAIL=root@amalthea
+EMAIL=root@amalthea.marmot.org
 PIKASERVER=wcpl.production
 OUTPUT_FILE="/var/log/vufind-plus/${PIKASERVER}/extract_and_reindex_output.log"
 
