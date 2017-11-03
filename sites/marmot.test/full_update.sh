29e10ce19d78b37f226892f1c2a6af615d96d0c3
pbrammeier <pascal@marmot.org>
2017-11-03 12:03:42 -0600

    fetch data for CMC overdrive sideload on test #D-1107

--- sites/marmot.test/full_update.sh
+++ sites/marmot.test/full_update.sh
@@ -99,6 +99,9 @@ cd /usr/local/vufind-plus/vufind/cron;./HOOPLA.sh ${PIKASERVER} >> ${OUTPUT_FILE
 # CMC Ebsco Academic Marc Updates
 /usr/local/vufind-plus/vufind/fetch_sideload_data.sh ${PIKASERVER} cmc/ebsco ebsco/cmc >> ${OUTPUT_FILE}
 
+#CMC Overdrive sideload
+/usr/local/vufind-plus/vufind/fetch_sideload_data.sh ${PIKASERVER} cmc/overdrive overdrive/cmc >> ${OUTPUT_FILE}
+
 # Adams Ebsco Marc Updates
 /usr/local/vufind-plus/vufind/fetch_sideload_data.sh ${PIKASERVER} adams/ebsco ebsco/adams >> ${OUTPUT_FILE}
 
