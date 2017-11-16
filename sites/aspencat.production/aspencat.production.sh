c22b7ccef7bd65d702004acf89646b2be39d79c9
pbrammeier <pascal@marmot.org>
2017-11-16 14:28:58 -0700

    update solr settings #D-1590

--- sites/aspencat.production/aspencat.production.sh
+++ sites/aspencat.production/aspencat.production.sh
@@ -13,6 +13,6 @@ fi
 
 if [[ ( "$1" == "start" ) || ( "$1" == "restart") ]]
 	then
-		../default/solr/bin/solr start -m 10g -p 8180 -s "/data/vufind-plus/aspencat.production/solr_master" -d "/usr/local/vufind-plus/sites/default/solr/jetty"
-		../default/solr/bin/solr start -m 9g -p 8080 -a "-Dsolr.masterport=8180" -s "/data/vufind-plus/aspencat.production/solr_searcher" -d "/usr/local/vufind-plus/sites/default/solr/jetty"
+		../default/solr/bin/solr start -m 8g -p 8180 -s "/data/vufind-plus/aspencat.production/solr_master" -d "/usr/local/vufind-plus/sites/default/solr/jetty"
+		../default/solr/bin/solr start -m 8g -p 8080 -a "-Dsolr.masterport=8180" -s "/data/vufind-plus/aspencat.production/solr_searcher" -d "/usr/local/vufind-plus/sites/default/solr/jetty"
 fi
