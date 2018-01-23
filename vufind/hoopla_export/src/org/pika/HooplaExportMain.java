6f1fe759397ae1fbe20bd2b781f601bcf53c4c22
Mark Noble <mark@marmot.org>
2018-01-23 16:57:58 -0700

    Start of hoopla export work

---
+++ vufind/hoopla_export/src/org/pika/HooplaExportMain.java
@@ -0,0 +1,117 @@
+package org.pika;
+
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
+import org.ini4j.Ini;
+import org.ini4j.InvalidFileFormatException;
+import org.ini4j.Profile;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Date;
+
+/**
+ * Created by mnoble on 1/23/2018.
+ */
+public class HooplaExportMain {
+	private static Logger logger = Logger.getLogger(HooplaExportMain.class);
+	private static String serverName;
+	public static void main(String[] args){
+		serverName = args[0];
+
+		Date startTime = new Date();
+		File log4jFile = new File("../../sites/" + serverName + "/conf/log4j.hoopla_extract.properties");
+		if (log4jFile.exists()){
+			PropertyConfigurator.configure(log4jFile.getAbsolutePath());
+		}else{
+			log4jFile = new File("../../sites/default/conf/log4j.hoopla_extract.properties");
+			if (log4jFile.exists()){
+				PropertyConfigurator.configure(log4jFile.getAbsolutePath());
+			}else{
+				System.out.println("Could not find log4j configuration " + log4jFile.toString());
+			}
+		}
+		logger.info(startTime.toString() + ": Starting Hoopla Extract");
+
+		// Read the base INI file to get information about the server (current directory/cron/config.ini)
+		Ini ini = loadConfigFile("config.ini");
+	}
+
+	private static Ini loadConfigFile(String filename){
+		//First load the default config file
+		String configName = "../../sites/default/conf/" + filename;
+		logger.info("Loading configuration from " + configName);
+		File configFile = new File(configName);
+		if (!configFile.exists()) {
+			logger.error("Could not find configuration file " + configName);
+			System.exit(1);
+		}
+
+		// Parse the configuration file
+		Ini ini = new Ini();
+		try {
+			ini.load(new FileReader(configFile));
+		} catch (InvalidFileFormatException e) {
+			logger.error("Configuration file is not valid.  Please check the syntax of the file.", e);
+		} catch (FileNotFoundException e) {
+			logger.error("Configuration file could not be found.  You must supply a configuration file in conf called config.ini.", e);
+		} catch (IOException e) {
+			logger.error("Configuration file could not be read.", e);
+		}
+
+		//Now override with the site specific configuration
+		String siteSpecificFilename = "../../sites/" + serverName + "/conf/" + filename;
+		logger.info("Loading site specific config from " + siteSpecificFilename);
+		File siteSpecificFile = new File(siteSpecificFilename);
+		if (!siteSpecificFile.exists()) {
+			logger.error("Could not find server specific config file");
+			System.exit(1);
+		}
+		try {
+			Ini siteSpecificIni = new Ini();
+			siteSpecificIni.load(new FileReader(siteSpecificFile));
+			for (Profile.Section curSection : siteSpecificIni.values()){
+				for (String curKey : curSection.keySet()){
+					//logger.debug("Overriding " + curSection.getName() + " " + curKey + " " + curSection.get(curKey));
+					//System.out.println("Overriding " + curSection.getName() + " " + curKey + " " + curSection.get(curKey));
+					ini.put(curSection.getName(), curKey, curSection.get(curKey));
+				}
+			}
+			//Also load password files if they exist
+			String siteSpecificPassword = "../../sites/" + serverName + "/conf/config.pwd.ini";
+			logger.info("Loading password config from " + siteSpecificPassword);
+			File siteSpecificPasswordFile = new File(siteSpecificPassword);
+			if (siteSpecificPasswordFile.exists()) {
+				Ini siteSpecificPwdIni = new Ini();
+				siteSpecificPwdIni.load(new FileReader(siteSpecificPasswordFile));
+				for (Profile.Section curSection : siteSpecificPwdIni.values()){
+					for (String curKey : curSection.keySet()){
+						ini.put(curSection.getName(), curKey, curSection.get(curKey));
+					}
+				}
+			}
+		} catch (InvalidFileFormatException e) {
+			logger.error("Site Specific config file is not valid.  Please check the syntax of the file.", e);
+		} catch (IOException e) {
+			logger.error("Site Specific config file could not be read.", e);
+		}
+
+		return ini;
+	}
+
+	private static String cleanIniValue(String value) {
+		if (value == null) {
+			return null;
+		}
+		value = value.trim();
+		if (value.startsWith("\"")) {
+			value = value.substring(1);
+		}
+		if (value.endsWith("\"")) {
+			value = value.substring(0, value.length() - 1);
+		}
+		return value;
+	}
+}
