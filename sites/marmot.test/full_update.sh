80cfa7afd3ccd7fa5ea6081f126f3a3316e9fcb8
pbrammeier <pascal@marmot.org>
2018-03-15 14:14:35 -0600

    Fort Lewis Naxos Jazz test setup #D-1529
    https://marmot.myjetbrains.com/youtrack/issue/D-1529

--- sites/marmot.test/full_update.sh
+++ sites/marmot.test/full_update.sh
@@ -151,6 +151,7 @@ cd /usr/local/vufind-plus/vufind/cron;./HOOPLA.sh ${PIKASERVER} >> ${OUTPUT_FILE
 
 # Naxos Fort Lewis
 /usr/local/vufind-plus/vufind/cron/fetch_sideload_data.sh ${PIKASERVER} fortlewis_sideload/naxos naxos/fortlewis >> ${OUTPUT_FILE}
+/usr/local/vufind-plus/vufind/cron/fetch_sideload_data.sh ${PIKASERVER} fortlewis_sideload/naxos_jazz naxos/jazz/fortlewis >> ${OUTPUT_FILE}
 
 #Extracts for sideloaded eContent; settings defined in config.pwd.ini [Sideload]
 cd /usr/local/vufind-plus/vufind/cron; ./sideload.sh ${PIKASERVER}
