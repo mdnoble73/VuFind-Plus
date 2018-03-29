691c006fdb9e9b32dc1a37b33843af38a2049b15
Mark Noble <mark@marmot.org>
2018-03-29 11:00:00 -0600

    Process formats based on iType for LION #D-1999

--- vufind/reindexer/src/org/vufind/LionRecordProcessor.java
+++ vufind/reindexer/src/org/vufind/LionRecordProcessor.java
@@ -8,6 +8,7 @@ import org.marc4j.marc.Subfield;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 /**
@@ -83,5 +84,56 @@ class LionRecordProcessor extends IIIRecordProcessor {
 			}
 		}
 		return false;
+	}
+
+	@Override
+	public void loadPrintFormatInformation(RecordInfo recordInfo, Record record) {
+		HashMap<String, Integer> itemCountsByItype = new HashMap<>();
+		HashMap<String, String> itemTypeToFormat = new HashMap<>();
+		int mostUsedCount = 0;
+		String mostPopularIType = "";		//Get a list of all the formats based on the items
+		List<DataField> items = MarcUtil.getDataFields(record, itemTag);
+		for(DataField item : items){
+			Subfield iTypeSubField = item.getSubfield(iTypeSubfield);
+			if (iTypeSubField != null){
+				String iType = iTypeSubField.getData().toLowerCase();
+				if (itemCountsByItype.containsKey(iType)){
+					itemCountsByItype.put(iType, itemCountsByItype.get(iType) + 1);
+				}else{
+					itemCountsByItype.put(iType, 1);
+					//Translate the iType to see what formats we get.  Some item types do not have a format by default and use the default translation
+					//We still will want to record those counts.
+					String translatedFormat = translateValue("format", iType, recordInfo.getRecordIdentifier());
+					itemTypeToFormat.put(iType, translatedFormat);
+				}
+
+				if (itemCountsByItype.get(iType) > mostUsedCount){
+					mostPopularIType = iType;
+					mostUsedCount = itemCountsByItype.get(iType);
+				}
+			}
+		}
+
+		if (itemTypeToFormat.size() == 0 || itemTypeToFormat.get(mostPopularIType) == null || itemTypeToFormat.get(mostPopularIType).length() == 0){
+			//We didn't get any formats from the collections, get formats from the base method (007, 008, etc).
+			//logger.debug("All formats are books or there were no formats found, loading format information from the bib");
+			super.loadPrintFormatFromBib(recordInfo, record);
+		} else{
+			//logger.debug("Using default method of loading formats from iType");
+			recordInfo.addFormat(itemTypeToFormat.get(mostPopularIType));
+			String translatedFormatCategory = translateValue("format_category", mostPopularIType, recordInfo.getRecordIdentifier());
+			if (translatedFormatCategory != null) {
+				recordInfo.addFormatCategory(translatedFormatCategory);
+			}
+			Long formatBoost = 1L;
+			String formatBoostStr = translateValue("format_boost", mostPopularIType, recordInfo.getRecordIdentifier());
+			if (formatBoostStr == null){
+				formatBoostStr = translateValue("format_boost", itemTypeToFormat.get(mostPopularIType), recordInfo.getRecordIdentifier());
+			}
+			if (formatBoostStr != null && Util.isNumeric(formatBoostStr)) {
+				formatBoost = Long.parseLong(formatBoostStr);
+			}
+			recordInfo.setFormatBoost(formatBoost);
+		}
 	}
 }
